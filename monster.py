00687f5c90de305e1e19925d7e3d9de448584078
cjw21824 <coreyjwilkins@gmail.com>
2021-07-06 20:35:25 -0700

    Scrape Monster Info page for characteristics

---
+++ monster.py
@@ -0,0 +1,27 @@
+
+
+
+class Monster():
+    def __init__(self, monsterName, minLevel, maxLevel, monsterFamily, minHp, maxHp,minAp,maxAp,minMp, maxMp, minEarthRes, maxEarthRes, minWaterRes, maxWaterRes, minAirRes, maxAirRes, minFireRes, maxFireRes, minNeutralRes, maxNeutralRes ):
+        self.monsterName = monsterName
+        self.minLevel = minLevel
+        self.maxLevel = maxLevel
+        self.minMp = minMp
+        self.maxMp = maxMp
+        self.minAp = minAp
+        self.maxAp = maxAp
+        self.family = monsterFamily
+        self.minHp = minHp
+        self.maxHp = maxHp
+        self.minEarthRes = minEarthRes
+        self.maxEarthRes = maxEarthRes
+        self.minWaterRes = minWaterRes
+        self.maxWaterRes = maxWaterRes
+        self.minFireRes = minFireRes
+        self.maxFireRes = maxFireRes
+        self.minAirRes = minAirRes
+        self.maxAirRes = maxAirRes
+        self.minNeutralRes = minNeutralRes
+        self.maxNeutralRes = maxNeutralRes
+
+    
--- scraper.py
+++ scraper.py
@@ -1,11 +1,12 @@
-import queue
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 from queue import Queue
 import sys
+from monster import Monster
 from concurrent import futures
 import time
+import re
 
 def create_driver():
     if sys.platform == 'win32':
@@ -40,25 +41,68 @@ def get_link_to_monsters():
         time.sleep(5)
     driver.quit()
 
-def scrape_monster_pages(url):
+def parse_ranges(soup, stat):
+    result = soup.find(text=re.compile(stat))
+    div = result.parent
+    range = div.findChild('span')
+    rangeText = str.split(range.text, sep='to')
+    if len(rangeText) > 1:
+        begin = ''.join(re.findall('[0-9]',rangeText[0]))
+        end = ''.join(re.findall('[0-9]',rangeText[1]))
+    else:
+        begin = ''.join(re.findall('[0-9]',rangeText[0]))
+        end = begin
+    return (begin,end)
+
+def parse_level_ranges(levels):
+    if len(levels) > 1:
+        begin = ''.join(re.findall('[0-9]',levels[0]))
+        end = ''.join(re.findall('[0-9]',levels[1]))
+    else:
+        begin = ''.join(re.findall('[0-9]',levels[0]))
+        end = begin
+    return (begin,end)
+        
+
+def get_monster_info(url):
+    
     driver = create_driver()
-    driver.create_options
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'lxml')
-    #scrape everything monster related
+    name = soup.find('h1', {'class': 'ak-return-link'})
+    family = soup.find('div', {'class': 'col-xs-8 ak-encyclo-detail-type'})
+    family = family.findChild('span', recursive=False)
+    levelRange = soup.find('div', {'class': 'col-xs-4 text-right ak-encyclo-detail-level'},text=True)
+    minLevel, maxLevel = parse_level_ranges(str.split(levelRange.text, sep="to"))
+    minHp, maxHp = parse_ranges(soup, 'HP:')
+    minAp, maxAp = parse_ranges(soup, "AP:")
+    minMp, maxMp = parse_ranges(soup, "MP:")
+    minEarthRes,maxEarthRes = parse_ranges(soup, "Earth:")
+    minAirRes, maxAirRes = parse_ranges(soup, "Air:")
+    minFireRes, maxFireRes = parse_ranges(soup, "Fire:")
+    minWaterRes, maxWaterRes = parse_ranges(soup, "Water:")
+    minNeutralRes, maxNeutralRes = parse_ranges(soup, "Neutral:")
+    name = str.strip(name.text)
+    family = family.text
+    monster = Monster(name, minLevel, maxLevel, family, 
+    minHp, maxHp, minAp, maxAp, minMp, maxMp, minEarthRes,
+    maxEarthRes, minWaterRes, maxWaterRes, minAirRes, maxAirRes, 
+    minFireRes, maxFireRes, minNeutralRes, maxNeutralRes)
     driver.quit()
+    return monster
 
 #def write_images_to_os
 #def write_monster_characterstics_to_db
 
 #move get_links_to_Monsters into a non-blocking thread. Do same for get_link_to_items
 get_link_to_monsters()
-with futures.ThreadPoolExecutor(max_workers=5) as executer:
+with futures.ThreadPoolExecutor(max_workers=1) as executer:
     
     while not url_queue.empty():
         url = url_queue.get()
         done, not_done = futures.wait(future_to_url,return_when=futures.FIRST_COMPLETED)
-        future_to_url[executer.submit(scrape_monster_pages, url)] = url
+        future_to_url[executer.submit(get_monster_info, url)] = url
         for future in done:
            #put into database, write to file system
+           monster = future.result
            pass
