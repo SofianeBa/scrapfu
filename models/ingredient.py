d96259e1ce1ade4ce8ecb4d7ec8ea6be05ba897f
Corey Wilkins <15042658+cjw21824@users.noreply.github.com>
2021-08-03 16:26:06 -0700

    fix column type

--- models/ingredient.py
+++ models/ingredient.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy.types import String
+from sqlalchemy.types import Integer
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey, Column
 from models.base import Base
@@ -8,6 +8,6 @@ class Ingredient(Base):
     __tablename__ = 'ingredient'
     recipe_id = Column(ForeignKey('recipe.id'), primary_key=True)
     resource_id = Column(ForeignKey('resource.id'), primary_key=True) 
-    quantity = Column(String, nullable=False)
+    quantity = Column(Integer, nullable=False)
     recipe = relationship('Recipe',back_populates='resources')
     resource = relationship('Resource',back_populates='recipes')
